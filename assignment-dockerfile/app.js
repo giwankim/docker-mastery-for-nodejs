04d95e58d5708ef93862353227dda4af68f118e4
Giwan Kim <giwankim@gmail.com>
2022-11-20 16:38:56 +0900

    feat: write node Dockerfile

--- assignment-dockerfile/app.js
+++ assignment-dockerfile/app.js
@@ -1,41 +1,37 @@
-'use strict';
+"use strict";
 
-const Hapi = require('@hapi/hapi');
+const Hapi = require("@hapi/hapi");
 
 const server = Hapi.server({
-    port: 3000,
-    host: '0.0.0.0'
+  port: 3000,
+  host: "0.0.0.0",
 });
 
 server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, h) => {
-
-        request.logger.info('In handler %s', request.path);
-        return 'Hello, world!';
-    }
+  method: "GET",
+  path: "/",
+  handler: (request, h) => {
+    request.logger.info("In handler %s", request.path);
+    return "Hello, world!";
+  },
 });
 
 const init = async () => {
+  await server.register({
+    plugin: require("hapi-pino"),
+    options: {
+      prettyPrint: true,
+      logEvents: ["response", "onPostStart"],
+    },
+  });
 
-     await server.register({
-        plugin: require('hapi-pino'),
-        options: {
-            prettyPrint: true,
-            logEvents: ['response', 'onPostStart']
-        }
-    });
-
-
-    await server.start();
-    console.log(`Server running at: ${server.info.uri}`);
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
 };
 
-process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
 });
 
 init();
